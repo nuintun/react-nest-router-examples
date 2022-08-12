02822f1c30f389069c77f74c626b278e3e02408c
nuintun <nuintun@qq.com>
2022-08-12 11:17:53 +0800

    feat: Update code

--- Assets/js/pages/App.tsx
+++ Assets/js/pages/App.tsx
@@ -19,22 +19,30 @@ const CoursesDetails = lazy(() => import('/js/pages/courses/Details'));
 
 const routes: Route<{ id: number }, 'id'>[] = [
   {
+    path: '/',
     element: <Navigation />,
     children: [
-      { path: '/login', meta: { id: 1 }, element: <Login /> },
       {
-        path: '/',
+        path: 'login',
+        meta: { id: 1 },
+        element: <Login />
+      },
+      {
         meta: { id: 2 },
         element: <Layout />,
         children: [
-          { index: true, meta: { id: 6 }, element: <Home /> },
+          {
+            index: true,
+            meta: { id: 6 },
+            element: <Home />
+          },
           {
             path: 'courses',
             meta: { id: 3 },
             element: <Courses />,
             children: [
               { index: true, meta: { id: 4 }, element: <CoursesIndex /> },
-              { path: '/courses/:id', meta: { id: 5 }, element: <CoursesDetails /> }
+              { path: ':id', meta: { id: 5 }, element: <CoursesDetails /> }
             ]
           }
         ]
